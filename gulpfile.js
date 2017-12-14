02e62b9a5d60d6895fc34fe440aa7d4bf9b1a11b
tellis002c <tellis002c@cable.comcast.com>
2017-12-14 13:03:22 -0500

    add pre-commit hooks

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,16 @@ var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-gulp.task('pre-commit', function () {
-  return guppy.src('pre-commit', function () {
-    return gulp.src('scripts/**/*.js')
-      .pipe(babel({ presets: ['es2015'] }))
-      .pipe(concat('bundle.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest(''));
-  });
+gulp.task('js', function () {
+  return gulp.src('scripts/**/*.js')
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(''));
 });
 
-gulp.task('default', function () {
-  return gulp.src('scripts/**/*.js')
-        .pipe(babel({presets: ['es2015']}))
-        .pipe(concat('bundle.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(''));
+gulp.task('pre-commit', function () {
+  return guppy.src('pre-commit', ['js']);
 });
+
+gulp.task('default', ['js']);
