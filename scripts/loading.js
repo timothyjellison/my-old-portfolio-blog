1490d69c8e21f0e6546d5122c475bea3dd7795f3
Timothy Ellison <timothyellison85@gmail.com>
2018-01-20 22:31:31 -0500

    Lessen the intervals background color updates from 1/2 second to 10.

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,14 +1,18 @@
 (function() {
   var header = document.getElementById('header');
-
-  var bgHue = 0;
-
-  setInterval(function() {
-    bgHue += bgHue < 359 ? 1 : -359;
-    header.style.backgroundColor = 'hsl(' + bgHue + ', 60%, 40%)';
-  }, 500);
-
   var mainContent = document.getElementById('mainContent');
+
+  var updateHeaderHue = (function() {
+    var bgHue = 0;
+
+    return function() {
+      bgHue += bgHue >= 360 ? -360 : 20;
+      header.style.backgroundColor = 'hsl(' + bgHue + ', 60%, 40%)';
+    }
+  })();
+
+  updateHeaderHue();
+  setInterval(updateHeaderHue, 10000);
 
   window.addEventListener('scroll', function(e) {
     if (window.scrollY >= (mainContent.offsetTop - 500)) {
