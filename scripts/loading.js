f2adea85196c555dd7e798e759a8f1db9abd5f72
Timothy Ellison <timothyellison85@gmail.com>
2018-03-27 06:30:25 -0400

    Fewer words, ditch the ugly contact form, no more rainbow effect

--- scripts/loading.js
+++ scripts/loading.js
@@ -2,24 +2,6 @@
   var headerNav = document.getElementById('headerNav');
   var navburgerContainer = document.getElementById('navburgerContainer');
   var navburger = document.getElementById('navburger');
-  var bgboiz = document.querySelectorAll('[data-bgboy]');
-
-  var updateBgColor = (function() {
-    var hue = 0;
-
-    return function() {
-      hue += hue >= 360 ? -360 : 20;
-
-      for (var i = 0; i < bgboiz.length; i++) {
-        var lightness = bgboiz[i].dataset.bgboy === "dark" ? '30%' : '40%';
-
-        bgboiz[i].style.backgroundColor = 'hsl(' + hue + ', 60%, ' + lightness + ')';
-      }
-    }
-  })();
-
-  updateBgColor()
-  setInterval(updateBgColor, 10000);
 
   navburger.addEventListener('click', function(e) {
     var action = navburgerContainer.classList.contains('mobile-nav-open') ? 'remove' : 'add';
